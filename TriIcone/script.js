230ef27f0e336006d7c75bf6b9d86b39b28e9b0b
laura <laura.duret.42@gmail.com>
2023-11-23 20:04:52 +0100

    gr TI ok

---
+++ TriIcone/script.js
@@ -0,0 +1,43 @@
+let consignes = ["rechercher une comédie française qui a une note de 2/10", "Consigne 2", "Consigne 3", /* autres consignes */];
+var indexConsigne = 0; // Initialisation de la variable globale
+
+function afficherConsigne() {
+    console.log('indexConsigne:', indexConsigne);
+    console.log('consigne:', consignes[indexConsigne]);
+    document.getElementById('instruction').innerText = consignes[indexConsigne];
+}
+
+window.addEventListener('load', afficherConsigne);
+
+function rediriger() {
+    console.log("La fonction rediriger a été appelée");
+
+    // Vérifie le nom de la page
+    if (window.location.pathname.endsWith("indexFinTI.html")) {
+        console.log("La fonction rediriger a été appelée, je suis dans la condition");
+
+        // Incrémente l'index
+        indexConsigne = (indexConsigne + 1) % consignes.length;
+
+        // Redirige vers la nouvelle page
+        window.location.href = "../genreTriIcone/indexGenreTI.html";
+        window.addEventListener('load', afficherConsigne);
+
+    }
+}
+
+function questionnaire() {
+    // Redirige vers le questionnaire
+    window.location.href = "formTriIcone.html";
+}
+
+function terminer() {
+    console.log(" je suis dans la fonction terminer")
+    // Redirige vers le questionnaire
+    window.location.href = "../../Accueil/exit2.html";
+}
+function afficherBoutonApresDelai() {
+    var bouton = document.getElementById("boutonTerminer");
+    bouton.style.display = "block";
+}
+
