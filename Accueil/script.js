b2bc5ab328e726a471639c0dfd0f5cc214afdb2d
Aurore <112873099+AurorePhl@users.noreply.github.com>
2023-11-23 15:16:18 +0100

    Add files via upload

--- Accueil/script.js
+++ Accueil/script.js
@@ -1,7 +1,9 @@
 let id = document.getElementById("id");
-let aleaId=Math.random().toString(36).substring(0, 6);
-id.innerHTML+=aleaId;
-let groupe;
+let grp=document.getElementById("grp");
+grp.innerHTML+=localStorage.getItem("groupe");
+localStorage.setItem("aleaId", Math.random().toString(36).substring(0,6));
+id.innerHTML+=localStorage.getItem("aleaId");
+ 
 function fin(){
     console.log("La fonction fin a été appelée");
     // Ferme la page et ne commence pas le test
@@ -16,16 +18,15 @@ function rediriger(){
     // Vérifie le nom de la page
     if (window.location.pathname.endsWith("formConsentement.html")) {
         console.log("La fonction rediriger a été appelée, je suis dans la condition");
-        let aleaGroupe=Math.random()*(5-1)+1;
-        if (aleaGroupe==1){
+        if (groupe==1){
             // Redirige vers la nouvelle page
             window.location.href = "../AleaIcone/FinAleaIcone/indexFinAI.html";
         }
-        else if (aleaGroupe==2){
+        else if (groupe==2){
             // Redirige vers la nouvelle page
             window.location.href = "../AleaTexte/genreAleaTexte/index.html";
         }
-        else if(aleaGroupe==3){
+        else if(groupe==3){
             // Redirige vers la nouvelle page
             window.location.href = "../TriIcone/genreTriIcone/index.html";
         }
@@ -40,7 +41,23 @@ if (window.location.pathname.endsWith("accueil.html")) {
 console.log("La fonction rediriger a été appelée, je suis dans la condition");
 
 // affecter groupe 
-groupe=document.getElementById("groupe").textContent;
+var grp1=document.getElementById('groupe1').value;
+var grp2=document.getElementById('groupe2').value;
+var grp3=document.getElementById('groupe3').value;
+var grp4=document.getElementById('groupe4').value;
+
+if(grp1.checked==true){
+    localStorage.setItem("groupe",1);
+}
+if(grp2.checked==true){
+    localStorage.setItem("groupe",2);
+}
+if(grp3.checked==true){
+    localStorage.setItem("groupe",3);
+}
+if(grp4.checked==true){
+    localStorage.setItem("groupe",4);
+}
 
 // redirection 
 window.location.href="formConsentement.html";
